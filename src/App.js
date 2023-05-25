e57e6778e98f499d361d7785759d92d07bbcb755
Fenix <arh.fenix@gmail.com>
2023-05-26 01:32:42 +0300

    done_04

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Main from './pages/main/Main';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Photodiary from './pages/photodiary/Photodiary';
 import Footer from './pages/footer/Footer';
+import ScrollToTop from './utils/ScrollToTop';
 import './style.css';
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <ScrollToTop/>
         <div className="container">
           <Nav />
         </div>
